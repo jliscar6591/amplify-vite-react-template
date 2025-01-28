874677a3fba6211ce7c1713c609249cbeae97d02
John Liscar <john.liscar@gmail.com>
2025-01-28 13:44:03 -0500

    changing styling and experimenting with routing and capacitor

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,25 @@ import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
+import './styles.css';
+
+const components = {
+  SignIn: {
+    Header() {
+      return <h2 style={{padding: '0 32px'}}>Custom Sign-In Header</h2>;
+    },
+    Footer() {
+      return <p>Custom Sign-In Footer</p>;
+    },
+  },
+};
 
 Amplify.configure(outputs);
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator components={components}>
       <App />
     </Authenticator>
   </React.StrictMode>
